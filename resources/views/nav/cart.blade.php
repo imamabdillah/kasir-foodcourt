376e05e13d5aab5fa1cb81af378302568c05a32e
Imam Abdillah <92290516+imamabdillah@users.noreply.github.com>
2023-10-20 04:00:41 +0700

    misah

---
+++ resources/views/nav/cart.blade.php
@@ -0,0 +1,243 @@
+<div class="flex-row-auto w-xl-425px">
+    <!--begin::Pos order-->
+    <div class="card card-flush bg-body" id="kt_pos_form">
+        <!--begin::Header-->
+        <div class="card-header pt-5">
+            <h3 class="card-title fw-bold text-gray-800 fs-2qx">Current Order</h3>
+            <!--begin::Toolbar-->
+            <div class="card-toolbar">
+                <a href="#" class="btn btn-light-primary fs-4 fw-bold py-4">Clear All</a>
+            </div>
+            <!--end::Toolbar-->
+        </div>
+        <!--end::Header-->
+        <!--begin::Body-->
+        <div class="card-body pt-0">
+            <!--begin::Table container-->
+            <div class="table-responsive mb-8">
+                <!--begin::Table-->
+                <table class="table align-middle gs-0 gy-4 my-0">
+                    <!--begin::Table head-->
+                    <thead>
+                        <tr>
+                            <th class="min-w-175px"></th>
+                            <th class="w-125px"></th>
+                            <th class="w-60px"></th>
+                        </tr>
+                    </thead>
+                    <!--end::Table head-->
+                    <!--begin::Table body-->
+                    <tbody>
+                        <tr data-kt-pos-element="item" data-kt-pos-item-price="33">
+                            <td class="pe-0">
+                                <div class="d-flex align-items-center">
+                                    <img src="assets/media/stock/food/img-2.jpg" class="w-50px h-50px rounded-3 me-3" alt="" />
+                                    <span class="fw-bold text-gray-800 cursor-pointer text-hover-primary fs-6 me-1">T-Bone Stake</span>
+                                </div>
+                            </td>
+                            <td class="pe-0">
+                                <!--begin::Dialer-->
+                                <div class="position-relative d-flex align-items-center" data-kt-dialer="true" data-kt-dialer-min="1" data-kt-dialer-max="10" data-kt-dialer-step="1" data-kt-dialer-decimals="0">
+                                    <!--begin::Decrease control-->
+                                    <button type="button" class="btn btn-icon btn-sm btn-light btn-icon-gray-400" data-kt-dialer-control="decrease">
+                                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr089.svg-->
+                                        <span class="svg-icon svg-icon-3x">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <rect x="6" y="11" width="12" height="2" rx="1" fill="currentColor" />
+                                            </svg>
+                                        </span>
+                                        <!--end::Svg Icon-->
+                                    </button>
+                                    <!--end::Decrease control-->
+                                    <!--begin::Input control-->
+                                    <input type="text" class="form-control border-0 text-center px-0 fs-3 fw-bold text-gray-800 w-30px" data-kt-dialer-control="input" placeholder="Amount" name="manageBudget" readonly="readonly" value="2" />
+                                    <!--end::Input control-->
+                                    <!--begin::Increase control-->
+                                    <button type="button" class="btn btn-icon btn-sm btn-light btn-icon-gray-400" data-kt-dialer-control="increase">
+                                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr087.svg-->
+                                        <span class="svg-icon svg-icon-3x">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <rect opacity="0.5" x="11" y="18" width="12" height="2" rx="1" transform="rotate(-90 11 18)" fill="currentColor" />
+                                                <rect x="6" y="11" width="12" height="2" rx="1" fill="currentColor" />
+                                            </svg>
+                                        </span>
+                                        <!--end::Svg Icon-->
+                                    </button>
+                                    <!--end::Increase control-->
+                                </div>
+                                <!--end::Dialer-->
+                            </td>
+                            <td class="text-end">
+                                <span class="fw-bold text-primary fs-2" data-kt-pos-element="item-total">$66.00</span>
+                            </td>
+                        </tr>
+                        <tr data-kt-pos-element="item" data-kt-pos-item-price="7.5">
+                            <td class="pe-0">
+                                <div class="d-flex align-items-center">
+                                    <img src="assets/media/stock/food/img-9.jpg" class="w-50px h-50px rounded-3 me-3" alt="" />
+                                    <span class="fw-bold text-gray-800 cursor-pointer text-hover-primary fs-6 me-1">Soup of the Day</span>
+                                </div>
+                            </td>
+                            <td class="pe-0">
+                                <!--begin::Dialer-->
+                                <div class="position-relative d-flex align-items-center" data-kt-dialer="true" data-kt-dialer-min="1" data-kt-dialer-max="10" data-kt-dialer-step="1" data-kt-dialer-decimals="0">
+                                    <!--begin::Decrease control-->
+                                    <button type="button" class="btn btn-icon btn-sm btn-light btn-icon-gray-400" data-kt-dialer-control="decrease">
+                                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr089.svg-->
+                                        <span class="svg-icon svg-icon-3x">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <rect x="6" y="11" width="12" height="2" rx="1" fill="currentColor" />
+                                            </svg>
+                                        </span>
+                                        <!--end::Svg Icon-->
+                                    </button>
+                                    <!--end::Decrease control-->
+                                    <!--begin::Input control-->
+                                    <input type="text" class="form-control border-0 text-center px-0 fs-3 fw-bold text-gray-800 w-30px" data-kt-dialer-control="input" placeholder="Amount" name="manageBudget" readonly="readonly" value="1" />
+                                    <!--end::Input control-->
+                                    <!--begin::Increase control-->
+                                    <button type="button" class="btn btn-icon btn-sm btn-light btn-icon-gray-400" data-kt-dialer-control="increase">
+                                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr087.svg-->
+                                        <span class="svg-icon svg-icon-3x">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <rect opacity="0.5" x="11" y="18" width="12" height="2" rx="1" transform="rotate(-90 11 18)" fill="currentColor" />
+                                                <rect x="6" y="11" width="12" height="2" rx="1" fill="currentColor" />
+                                            </svg>
+                                        </span>
+                                        <!--end::Svg Icon-->
+                                    </button>
+                                    <!--end::Increase control-->
+                                </div>
+                                <!--end::Dialer-->
+                            </td>
+                            <td class="text-end">
+                                <span class="fw-bold text-primary fs-2" data-kt-pos-element="item-total">$7.50</span>
+                            </td>
+                        </tr>
+                        <tr data-kt-pos-element="item" data-kt-pos-item-price="13.5">
+                            <td class="pe-0">
+                                <div class="d-flex align-items-center">
+                                    <img src="assets/media/stock/food/img-3.jpg" class="w-50px h-50px rounded-3 me-3" alt="" />
+                                    <span class="fw-bold text-gray-800 cursor-pointer text-hover-primary fs-6 me-1">Pancakes</span>
+                                </div>
+                            </td>
+                            <td class="pe-0">
+                                <!--begin::Dialer-->
+                                <div class="position-relative d-flex align-items-center" data-kt-dialer="true" data-kt-dialer-min="1" data-kt-dialer-max="10" data-kt-dialer-step="1" data-kt-dialer-decimals="0">
+                                    <!--begin::Decrease control-->
+                                    <button type="button" class="btn btn-icon btn-sm btn-light btn-icon-gray-400" data-kt-dialer-control="decrease">
+                                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr089.svg-->
+                                        <span class="svg-icon svg-icon-3x">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <rect x="6" y="11" width="12" height="2" rx="1" fill="currentColor" />
+                                            </svg>
+                                        </span>
+                                        <!--end::Svg Icon-->
+                                    </button>
+                                    <!--end::Decrease control-->
+                                    <!--begin::Input control-->
+                                    <input type="text" class="form-control border-0 text-center px-0 fs-3 fw-bold text-gray-800 w-30px" data-kt-dialer-control="input" placeholder="Amount" name="manageBudget" readonly="readonly" value="2" />
+                                    <!--end::Input control-->
+                                    <!--begin::Increase control-->
+                                    <button type="button" class="btn btn-icon btn-sm btn-light btn-icon-gray-400" data-kt-dialer-control="increase">
+                                        <!--begin::Svg Icon | path: icons/duotune/arrows/arr087.svg-->
+                                        <span class="svg-icon svg-icon-3x">
+                                            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                <rect opacity="0.5" x="11" y="18" width="12" height="2" rx="1" transform="rotate(-90 11 18)" fill="currentColor" />
+                                                <rect x="6" y="11" width="12" height="2" rx="1" fill="currentColor" />
+                                            </svg>
+                                        </span>
+                                        <!--end::Svg Icon-->
+                                    </button>
+                                    <!--end::Increase control-->
+                                </div>
+                                <!--end::Dialer-->
+                            </td>
+                            <td class="text-end">
+                                <span class="fw-bold text-primary fs-2" data-kt-pos-element="item-total">$27.00</span>
+                            </td>
+                        </tr>
+                    </tbody>
+                    <!--end::Table body-->
+                </table>
+                <!--end::Table-->
+            </div>
+            <!--end::Table container-->
+            <!--begin::Summary-->
+            <div class="d-flex flex-stack bg-success rounded-3 p-6 mb-11">
+                <!--begin::Content-->
+                <div class="fs-6 fw-bold text-white">
+                    <span class="d-block lh-1 mb-2">Subtotal</span>
+                    <span class="d-block mb-2">Discounts</span>
+                    <span class="d-block mb-9">Tax(12%)</span>
+                    <span class="d-block fs-2qx lh-1">Total</span>
+                </div>
+                <!--end::Content-->
+                <!--begin::Content-->
+                <div class="fs-6 fw-bold text-white text-end">
+                    <span class="d-block lh-1 mb-2" data-kt-pos-element="total">$100.50</span>
+                    <span class="d-block mb-2" data-kt-pos-element="discount">-$8.00</span>
+                    <span class="d-block mb-9" data-kt-pos-element="tax">$11.20</span>
+                    <span class="d-block fs-2qx lh-1" data-kt-pos-element="grant-total">$93.46</span>
+                </div>
+                <!--end::Content-->
+            </div>
+            <!--end::Summary-->
+            <!--begin::Payment Method-->
+            <div class="m-0">
+                <!--begin::Title-->
+                <h1 class="fw-bold text-gray-800 mb-5">Payment Method</h1>
+                <!--end::Title-->
+                <!--begin::Radio group-->
+                <div class="d-flex flex-equal gap-5 gap-xxl-9 px-0 mb-12" data-kt-buttons="true" data-kt-buttons-target="[data-kt-button]">
+                    <!--begin::Radio-->
+                    <label class="btn bg-light btn-color-gray-600 btn-active-text-gray-800 border border-3 border-gray-100 border-active-primary btn-active-light-primary w-100 px-4" data-kt-button="true">
+                        <!--begin::Input-->
+                        <input class="btn-check" type="radio" name="method" value="0" />
+                        <!--end::Input-->
+                        <!--begin::Icon-->
+                        <i class="fonticon-cash-payment fs-2hx mb-2 pe-0"></i>
+                        <!--end::Icon-->
+                        <!--begin::Title-->
+                        <span class="fs-7 fw-bold d-block">Cash</span>
+                        <!--end::Title-->
+                    </label>
+                    <!--end::Radio-->
+                    <!--begin::Radio-->
+                    <label class="btn bg-light btn-color-gray-600 btn-active-text-gray-800 border border-3 border-gray-100 border-active-primary btn-active-light-primary w-100 px-4 active" data-kt-button="true">
+                        <!--begin::Input-->
+                        <input class="btn-check" type="radio" name="method" value="1" />
+                        <!--end::Input-->
+                        <!--begin::Icon-->
+                        <i class="fonticon-card fs-2hx mb-2 pe-0"></i>
+                        <!--end::Icon-->
+                        <!--begin::Title-->
+                        <span class="fs-7 fw-bold d-block">Card</span>
+                        <!--end::Title-->
+                    </label>
+                    <!--end::Radio-->
+                    <!--begin::Radio-->
+                    <label class="btn bg-light btn-color-gray-600 btn-active-text-gray-800 border border-3 border-gray-100 border-active-primary btn-active-light-primary w-100 px-4" data-kt-button="true">
+                        <!--begin::Input-->
+                        <input class="btn-check" type="radio" name="method" value="2" />
+                        <!--end::Input-->
+                        <!--begin::Icon-->
+                        <i class="fonticon-mobile-payment fs-2hx mb-2 pe-0"></i>
+                        <!--end::Icon-->
+                        <!--begin::Title-->
+                        <span class="fs-7 fw-bold d-block">E-Wallet</span>
+                        <!--end::Title-->
+                    </label>
+                    <!--end::Radio-->
+                </div>
+                <!--end::Radio group-->
+                <!--begin::Actions-->
+                <button class="btn btn-primary fs-1 w-100 py-4">Print Bills</button>
+                <!--end::Actions-->
+            </div>
+            <!--end::Payment Method-->
+        </div>
+        <!--end: Card Body-->
+    </div>
+    <!--end::Pos order-->
+</div>
